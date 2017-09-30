76bc48e72f87042cae6720a36bcb2c2da86f2c11
Michael Pratt <michael@pratt.im>
2017-09-30 15:47:45 -0700

    Build client

--- cmd/build-release/main.go
+++ cmd/build-release/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/golang/glog"
 )
@@ -45,6 +46,45 @@ func buildRestic(root, release string) error {
 	return nil
 }
 
+func buildClient(root, release string) error {
+	glog.Infof("Determing client version...")
+
+	cmd := exec.Command("git", "describe", "--long", "--tags", "--dirty", "--always")
+	b, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("error finding version: %v", err)
+	}
+
+	version := strings.Trim(string(b), "\r\n")
+	glog.Infof("Client version: %s", version)
+
+	ldflag := fmt.Sprintf(`-X "main.versionStr=%s"`, version)
+
+	bin := filepath.Join(release, "client")
+	glog.Infof("Building %s", bin)
+
+	cmd = exec.Command("go", "build", "-o", bin, "-ldflags", ldflag, "github.com/prattmic/restic-remote/cmd/client")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error building client: %v", err)
+	}
+
+	bin = filepath.Join(release, "client.exe")
+	glog.Infof("Building %s", bin)
+
+	cmd = exec.Command("go", "build", "-o", bin, "-ldflags", ldflag, "github.com/prattmic/restic-remote/cmd/client")
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "GOOS=windows")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error building client.exe: %v", err)
+	}
+
+	return nil
+}
+
 func main() {
 	flag.Set("alsologtostderr", "true")
 	flag.Parse()
@@ -62,5 +102,9 @@ func main() {
 
 	if err := buildRestic(root, release); err != nil {
 		glog.Exitf("Unable to build restic: %v", err)
+	}
+
+	if err := buildClient(root, release); err != nil {
+		glog.Exitf("Unable to build client: %v", err)
 	}
 }
