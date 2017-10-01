612e9a52e7a696784bb00aacad07ac881eb10c75
Michael Pratt <michael@pratt.im>
2017-09-30 17:48:38 -0700

    Add binary post support

--- server/binary.go
+++ server/binary.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -21,13 +22,19 @@ type binaryEntity struct {
 }
 
 func binary(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-
-	if r.Method != "GET" {
+	switch r.Method {
+	case "GET":
+		binaryGet(w, r)
+	case "POST":
+		binaryGet(w, r)
+	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "Only GET requests allowed")
-		return
+		fmt.Fprintf(w, "%s requests not allowed", r.Method)
 	}
+}
+
+func binaryGet(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
 
 	name := r.FormValue("name")
 	if name == "" {
@@ -54,6 +61,61 @@ func binary(w http.ResponseWriter, r *http.Request) {
 
 	if err := json.NewEncoder(w).Encode(be[0].Binary); err != nil {
 		log.Printf("Failed to encode binary info %+v: %v", be[0].Binary, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Internal Server Error")
+		return
+	}
+}
+
+func binaryPost(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Failed to read body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Internal Server Error")
+		return
+	}
+
+	var bin api.Binary
+	if err := json.Unmarshal(b, &bin); err != nil {
+		log.Printf("Failed to decode binary %q: %v", string(b), err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Malformed binary")
+		return
+	}
+
+	if bin.Name == "" {
+		log.Printf("Binary %+v missing name", bin)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "name required")
+		return
+	}
+
+	if bin.Path == "" {
+		log.Printf("Binary %+v missing path", bin)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "path required")
+		return
+	}
+
+	if bin.Version == "" {
+		log.Printf("Binary %+v missing version", bin)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "version required")
+		return
+	}
+
+	entity := binaryEntity{
+		Timestamp: time.Now(),
+		Binary:    bin,
+	}
+
+	// Stash the binary in datastore.
+	key := datastore.NewIncompleteKey(ctx, "Binary", nil)
+	if _, err := datastore.Put(ctx, key, &entity); err != nil {
+		log.Printf("Failed to store binary: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Internal Server Error")
 		return
