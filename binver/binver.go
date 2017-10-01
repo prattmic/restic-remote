2535a03057983391a2b1b7e89b8b53839f6a62da
Michael Pratt <michael@pratt.im>
2017-10-01 16:23:17 -0700

    Only look at the first line of restic version

--- binver/binver.go
+++ binver/binver.go
@@ -14,6 +14,14 @@ func Client(bin string) (string, error) {
 func Restic(bin string) (string, error) {
 	cmd := exec.Command(bin, "version")
 	b, err := cmd.Output()
-	return strings.Trim(string(b), "\r\n"), err
+	if err != nil {
+		return "", err
+	}
+
+	// Only look at the first line. The second line contains the system the
+	// binary is running on, which isn't relevant.
+	s := strings.Trim(string(b), "\r\n")
+	s = strings.SplitN(s, "\n", 2)[0]
+	return strings.Trim(s, "\r\n"), nil
 }
 
